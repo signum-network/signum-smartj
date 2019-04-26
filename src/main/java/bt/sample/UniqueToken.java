affabd8fafa305aa54e2eb70791df3a9abcbde53
jjos <jjos2372@gmail.com>
2019-04-25 22:32:44 -0300

    Playing with NFT

--- src/main/java/bt/sample/UniqueToken.java
+++ src/main/java/bt/sample/UniqueToken.java
@@ -2,15 +2,17 @@ package bt.sample;
 
 import bt.Contract;
 import bt.EmulatorWindow;
+import bt.Timestamp;
 import bt.Address;
 
 /**
- * A unique token smart contract.
+ * A unique (non-fungible) token smart contract.
  * 
  * This contract represents a non-fungible or unique token for Burst blockchain.
  * 
- * When created it is owned by the creator. After that it can be transfered
- * to other addresses. Only the owner can transfer the ownership.
+ * When created it is owned by the creator. After that the owner can transfer
+ * the ownership to another address.
+ * Additionally, the owner can put the token on sale with a given timeout.
  *
  * Inspired by http://erc721.org/
  * 
@@ -19,6 +21,8 @@ import bt.Address;
 public class UniqueToken extends Contract {
 	
 	Address owner;
+	long price;
+	Timestamp timeout;
 
 	/**
 	 * Constructor, when in blockchain the constructor is called when the first TX
@@ -40,6 +44,35 @@ public class UniqueToken extends Contract {
 		if(owner.equals(this.getCurrentTx().getSenderAddress())){
 			// only owner can transfer ownership
 			owner = newOwner;
+			price = 0; // not for sale
+		}
+	}
+
+	/**
+	 * Put this toke on sale for the given price.
+	 * 
+	 * @param price the price (buyer need to transfer at least this amount)
+	 * @param timeout how many minutes the sale will be available
+	 */
+	public void sell(long price, long timeout){
+		this.price = price;
+		this.timeout = getBlockTimestamp().addMinutes(timeout);
+	}
+
+	/**
+	 * Buy an on sale token.
+	 * Buyer need to transfer the asked price
+	 */
+	public void buy(){
+		if(price>0 && getBlockTimestamp().le(timeout) && getCurrentTx().getAmount() >= price){
+			// conditions match, let's execute the sale
+			sendAmount(price, owner); // pay the current owner
+			owner = getCurrentTx().getSenderAddress(); // new owner
+			price = 0; // not for sale
+		}
+		else {
+			// send back funds of an invalid order
+			sendAmount(getCurrentTx().getAmount(), getCurrentTx().getSenderAddress());
 		}
 	}
 
