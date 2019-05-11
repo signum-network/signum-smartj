af6d0e97d7dcac6db565bca73a3ff423e1821d72
Harry Phillips <harryjamesphillips@gmail.com>
2019-05-11 01:26:00 +0100

    Comment

--- src/main/java/bt/CompilerVersion.java
+++ src/main/java/bt/CompilerVersion.java
@@ -2,5 +2,5 @@ package bt;
 
 public enum CompilerVersion {
     // Must be ordered correctly in order to detect whether version is newer or older than another
-    v0_0_0
+    v0_0_0 // Initial pre-alpha development version.
 }
