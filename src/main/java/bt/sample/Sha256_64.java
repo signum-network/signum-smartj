59b44f30fc9c5fc824c720fc2c4400af66289e54
jjos <jjos2372@gmail.com>
2019-06-30 11:43:55 -0300

    test for hash functions

---
+++ src/main/java/bt/sample/Sha256_64.java
@@ -0,0 +1,30 @@
+package bt.sample;
+
+import bt.*;
+import bt.compiler.CompilerVersion;
+import bt.compiler.TargetCompilerVersion;
+import bt.ui.EmulatorWindow;
+
+/**
+ * A smart contract that simply echoes the message received.
+ * 
+ * @author jjos
+ */
+@TargetCompilerVersion(CompilerVersion.v0_0_0)
+public class Sha256_64 extends Contract {
+
+    long sha256_64;
+	
+	/**
+	 * Any new transaction received will be handled by this function.
+	 */
+	public void txReceived(){
+		sha256_64 = this.performSHA256_64(getPrevBlockHash1(), getCurrentTxTimestamp().getValue());
+	}
+
+	public static void main(String[] args) {
+		new EmulatorWindow(Sha256_64.class);
+	}
+}
+
+
