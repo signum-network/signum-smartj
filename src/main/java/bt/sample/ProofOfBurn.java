d9a3ca4ea03b6fe9d028ed1316a4fa12d1bcc778
jjos <jjos2372@gmail.com>
2021-01-01 13:19:34 -0300

    Burn example.

---
+++ src/main/java/bt/sample/ProofOfBurn.java
@@ -0,0 +1,27 @@
+package bt.sample;
+
+import bt.Contract;
+import bt.compiler.CompilerVersion;
+import bt.compiler.TargetCompilerVersion;
+import bt.ui.EmulatorWindow;
+
+/**
+ * A smart contract that burns all BURST it receives.
+ * 
+ * @author jjos
+ */
+@TargetCompilerVersion(CompilerVersion.v0_0_0)
+public class ProofOfBurn extends Contract {
+	/**
+	 * Any new transaction received will be handled by this function.
+	 */
+	public void txReceived(){
+		sendAmount(getCurrentBalance(), getAddress(0L));
+	}
+
+	public static void main(String[] args) {
+		new EmulatorWindow(ProofOfBurn.class);
+		compile();
+	}
+}
+
