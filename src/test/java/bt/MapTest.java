407890bc86930b7644745fe4a28e8badff0ffe24
frankTheTank72 <39116221+frankTheTank72@users.noreply.github.com>
2023-05-30 20:22:05 +0200

    uPDATE

--- src/test/java/bt/MapTest.java
+++ src/test/java/bt/MapTest.java
@@ -1,84 +1,74 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+package bt;
 
-import signumj.entity.SignumAddress;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+import bt.compiler.Compiler;
+import bt.compiler.Compiler.Error;
+import bt.sample.MapSaveRead;
+import signumj.entity.SignumID;
 import signumj.entity.SignumValue;
-import signumj.entity.response.Transaction;
-import signumj.entity.response.TransactionAppendix;
-import signumj.response.appendix.PlaintextMessageAppendix;
-import signumj.service.NodeService;
-import signumj.service.impl.HttpBurstNodeService;
+import signumj.entity.response.AT;
+import signumj.entity.response.TransactionBroadcast;
 
 /**
- * Check the deadlines of a series of blocks and computes alternative deadlines.
- * 
- * @author jjos
+ * We assume a localhost testnet with 0 seconds mock mining is available for the
+ * tests to work.
  *
+ * @author jjos
  */
-public class BittrexTxs{
+public class MapTest extends BT {
 
-	public static void main(String[] args) throws Exception {
-		
-		NodeService ns = new HttpBurstNodeService("http://localhost:8125", "signumj", 20);
-		
-		SignumAddress address = SignumAddress.fromEither("S-HK9D-P74Q-XDEJ-D6PGM");
-		
-		BufferedWriter writer = new BufferedWriter(new FileWriter("bittrex.csv"));
+	static {
+		activateSIP37(true);
+	}
 
-		int ntxs = 400;
-		int start = 0;
-		int end = ntxs;
-		
-		long total = 0;
-
-		while(true) {
-			Transaction[] txs = ns.getAccountTransactions(address, start, end, false).blockingGet();
-			if(txs == null || txs.length == 0 || start > 2000)
-				break;
-			
-			System.out.println(end);
-			
-			for(Transaction tx : txs) {
-				TransactionAppendix append = null;
-				if(tx.getAppendages() != null && tx.getAppendages().length > 0)
-					append = tx.getAppendages()[0];
-				
-				String memo = "";
-				
-				if(append instanceof PlaintextMessageAppendix) {
-					PlaintextMessageAppendix appendMessage = (PlaintextMessageAppendix) append;
-
-					memo = appendMessage.getMessage();
-					
-					if(memo.equals("f324f4de3a8f4365ac248d2cbcc62e0a72d537e08f9c476093a48d826a3cec52")) {
-					  System.out.println("https://chain.signum.network/tx/" + tx.getId().getID());
-					  total += tx.getAmount().longValue();
-					}
-					else {
-					  continue;
-					}
-				}
-
-				writer.append(tx.getId().getID() + ",");
-				writer.append(tx.getTimestamp().getAsDate().toString() + ",");
-				if(tx.getSender().getSignedLongId() != address.getSignedLongId()) {
-					writer.append(tx.getSender() + ",");
-				}
-				else {					
-					writer.append(tx.getRecipient() + ",");
-					writer.append("-");
-				}
-				writer.append(tx.getAmount().toUnformattedString() + ",");
-				writer.append('"' + memo.replace('\n', ' ') + '"' + '\n');
-			}
-			
-			start = end + 1;
-			end += ntxs;
+	@Test
+	public void testAll() throws Exception {
+		Compiler comp = BT.compileContract(MapSaveRead.class);
+		for(Error e : comp.getErrors()) {
+			System.err.println(e.getMessage());
 		}
+		assertTrue(comp.getErrors().size() == 0);
+
+		AT contract1 = registerContract(MapSaveRead.class, SignumValue.fromSigna(0.3));
+		System.out.println("contract 1 :" + contract1.getId().getID());
+
+		AT contract2 = registerContract(MapSaveRead.class, SignumValue.fromSigna(0.3));
+		System.out.println("contract 2 :" + contract2.getId().getID());
 		
-		System.out.println("total: " + SignumValue.fromNQT(total).toString());
+		Random r = new Random();
+		long key1 = r.nextLong();
+		long key2 = r.nextLong();
+		long value = r.nextLong();
 		
-		writer.close();
-		ns.close();
+		System.out.println("key1: " + SignumID.fromLong(key1).getID());
+		System.out.println("key2: " + SignumID.fromLong(key2).getID());
+		System.out.println("value: " + SignumID.fromLong(value).getID());
+
+		TransactionBroadcast tb = BT.callMethod(BT.PASSPHRASE, contract1.getId(), comp.getMethod("saveValue"), contract1.getMinimumActivation(), SignumValue.fromSigna(0.1), 1000,
+				key1, key2, value);
+		forgeBlock(tb);
+		forgeBlock();
+		forgeBlock();
+
+		tb = BT.callMethod(BT.PASSPHRASE, contract1.getId(), comp.getMethod("readValue"), contract1.getMinimumActivation(), SignumValue.fromSigna(0.1), 1000,
+				key1, key2);
+		forgeBlock(tb);
+		forgeBlock();
+
+		long valueRead = BT.getContractFieldValue(contract1, comp.getFieldAddress("valueRead"));
+		assertEquals(value, valueRead);
+		
+		tb = BT.callMethod(BT.PASSPHRASE, contract2.getId(), comp.getMethod("readValueExternal"), contract1.getMinimumActivation(), SignumValue.fromSigna(0.1), 1000,
+				contract1.getId().getSignedLongId(), key1, key2);
+		forgeBlock(tb);
+		forgeBlock();
+		valueRead = BT.getContractFieldValue(contract2, comp.getFieldAddress("valueRead"));
+		assertEquals(value, valueRead);
 	}
 }
